c6020abf35006da7ace68ba1790a88319099b025
unknown <GZliew>
2018-05-29 14:47:58 +0800

    make it better

--- utils.py
+++ utils.py
@@ -8,50 +8,17 @@ client = Wit(access_token = access_token)
 
 def wit_response(message_text):
     resp = client.message(message_text)
-    categories = {'greetings':None, 'location':None}
+    entity = None
+    value = None
     ##extract name and value of entity
 
-    entities = list(resp['entities'])
-    
-    for entity in entities:
-        categories[entity] = resp['entities'][entity][0]['value']
-        
-    return categories
+    try:
+        entity = list(resp['entities'])[0]
+        value = resp['entities'][entity][0]['value']
+    except:
+        pass
+    return (entity, value)
 
-def send_generic_message(self, recipient_id, elements, notification_type=NotificationType.regular):
-        """Send generic messages to the specified recipient.
-        https://developers.facebook.com/docs/messenger-platform/send-api-reference/generic-template
-        Input:
-            recipient_id: recipient id to send to
-            elements: generic message elements to send
-        Output:
-            Response from API as <dict>
-        """
-        return self.send_message(recipient_id, {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements
-                }
-            }
-        }, notification_type)
-
-def get_reply(categories):
-
-    if categories['greetings'] != None:
-
-        element = {
-                'title': 'Hello'
-                'buttons': [{
-                    'type': 'web_url',
-                    'title': "Read More",
-                    'url': '#'
-                        }],
-                }
-
-    return element
-    
     
 ##print(wit_response("I live in Malaysia"))
 
