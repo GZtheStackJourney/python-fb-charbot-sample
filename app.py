2aa799789aa26ad79e7642db70199d52732837bb
unknown <GZliew>
2018-06-02 16:15:22 +0800

    make it better

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def webhook():
 
     # endpoint for processing incoming messaging events
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-    set_get_started_button_payload("get started")
-    
+        set_get_started_button_payload("get started")
+
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
