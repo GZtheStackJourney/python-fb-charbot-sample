462c01301765a42b5d4d4d24ac0b77f0552d66e3
unknown <GZliew>
2018-06-02 14:43:24 +0800

    make it better

--- app.py
+++ app.py
@@ -12,18 +12,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+            messenger.init_bot()
 
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        set_get_started_button_payload("get started")
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
-
-        if request.args.get('init') and request.args.get('init') == 'true': # get started button
-            set_get_started_button_payload("get started")
-            return ''
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
