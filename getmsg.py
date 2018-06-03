e113a66f32f3b633b950f06bc418a5ec790776d5
unknown <GZliew>
2018-06-03 11:21:50 +0800

    make it better

--- getmsg.py
+++ getmsg.py
@@ -24,23 +24,23 @@ def get_message(recipient_id, message_received):
 
 def get_response(recipient_id, message_received, message_received2=None):
 
-	x = message_received
+	payload_id = message_received
 	income = message_received2
 	profile_id = recipient_id
 
-	if x == "Q1 A":
+	if payload_id == "Q1 A":
 		response = "Are you active in social media?"
 		answers = [QuickReply("Yes", "Q2 A"), QuickReply("No", "Q2 B")]
-	elif x == "Q1 B":
+	elif payload_id == "Q1 B":
 		response = "Are you keen in social politics?"
 		answers = [QuickReply("Yes", "Q2 A"), QuickReply("No", "Q2 B")]
-	elif x == "Q2 A" or x == "Q2 B":
+	elif payload_id == "Q2 A" or payload_id == "Q2 B":
 		response = "Will you be willing to test our products?"
 		answers = [QuickReply("Yes", "Q3 A"), QuickReply("No", "Q3 B")]
-	elif x == "Q3 A":
+	elif payload_id == "Q3 A":
 		response = "Whats your yearly income range?"
 		answers = [QuickReply("30k - 100k", "Q4 A"), QuickReply("above 100k", "Q4 B")]
-	elif x == "Q3 B":
+	elif payload_id == "Q3 B" or payload_id == "Q4 A" or payload_id == "Q4 B":
 		response = "Thank you for taking part. For more info visit below."
 		answers = [ActionButton(ButtonType.WEB_URL, "Admin FB", "https://www.facebook.com/testpageauto123/")]
 		return send_buttons(profile_id, response, answers)
