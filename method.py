b32741f790c77a89b85692c91cc597f2d32c3bb5
unknown <GZliew>
2018-06-17 20:54:28 +0800

    make it better

--- method.py
+++ method.py
@@ -204,6 +204,27 @@ def send_quick_replies(recipient_id, message_text, reply_list):
             log(r.text)
 
 
+def typing(recipient_id, on=True):
+
+	sender_action = "typing_on" if on else "typing_off"
+
+	params = {
+		"access_token": os.environ["PAGE_ACCESS_TOKEN"]
+	}
+	headers = {
+		"Content-Type": "application/json"
+	}
+	data = json.dumps({
+		"recipient": {
+			"id": recipient_id
+		},
+		"sender_action": sender_action
+	})
+	r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+	if r.status_code != 200:
+		log(r.status_code)
+		log(r.text)
+
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
     try:
@@ -217,40 +238,3 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
     sys.stdout.flush()
 
 
-
-# def quick_replies(recipient_id, message_text):
-
-#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-#     params = {
-#         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     data = json.dumps({
-#         "recipient": {
-#             "id": recipient_id
-#         },
-#         "message": {
-#             "text": message_text,
-#             "quick_replies":[
-#               {
-#                 "content_type":"text",
-#                 "title":"Type A",
-#                 "payload":"<POSTBACK_PAYLOAD>",
-#                 # "image_url":"http://example.com/img/red.png"
-#               },
-#               {
-#                 "content_type":"text",
-#                 "title":"Type B",
-#                 "payload":"<POSTBACK_PAYLOAD>",
-#               }
-#             ]
-#         }
-#     })
-
-#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-#     if r.status_code != 200:
-#         log(r.status_code)
-#         # log(r.text)
