fb731dbbfe47136da327a34576d74adaa36473ff
unknown <GZliew>
2018-06-22 23:04:04 +0800

    make it better

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ def webhook():
 
     # endpoint for processing incoming messaging events
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		set_persistent_menu("start")
 		set_get_started_button_payload("get started")
+		set_persistent_menu("start")
 		set_greeting_text("Welcome!")
 
     data = request.get_json()
