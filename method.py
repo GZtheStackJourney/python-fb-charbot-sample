714c0bca407ba78fede8a2b8a0dd363dd28912b1
unknown <GZliew>
2018-06-22 22:24:13 +0800

    make it better

--- method.py
+++ method.py
@@ -238,7 +238,7 @@ def set_persistent_menu(Payload):
 			"persistent_menu":[
 				{
 					"locale":"default",
-					"composer_input_disabled": true,
+					"composer_input_disabled": false,
 					"call_to_actions":[
 						{
 							"title":"Do Survey",
