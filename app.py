ccf832767ebd9648ddf4acfca2c33aa49b8b5a6a
unknown <GZliew>
2018-06-24 08:18:48 +0800

    make it better

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def webhook():
 		# set_get_started_button_payload("get started")
 		set_greeting_text("Welcome!")
 		set_get_started_menu("get started")
-        set_persistent_menu("start")
+		set_persistent_menu("start")
 
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
