db4ae004f527745353319da1bb8daeea88259c63
unknown <GZliew>
2018-07-15 14:56:36 +0800

    make it better

--- method.py
+++ method.py
@@ -225,6 +225,31 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def send_image(recipient_id, image):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "image",
+                "payload": {
+                    "url": image
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
 def send_quick_replies(recipient_id, message_text, reply_list):
         log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
