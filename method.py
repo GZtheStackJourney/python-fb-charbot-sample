ae344ed1859f070fb535e134769a890ea9743613
unknown <GZliew>
2018-09-10 23:43:37 +0800

    make it better

--- method.py
+++ method.py
@@ -281,7 +281,7 @@ def send_quick_replies(recipient_id, message_text, reply_list):
 def typing(recipient_id, on=True):
 
 	sender_action = "typing_on" if on else "typing_off"
-
+    log("robot typing")
 	params = {
 		"access_token": os.environ["PAGE_ACCESS_TOKEN"]
 	}
